be588204eebdfdfdaeeb009513729154ae17d7af
Steven H. Schwartz <steven.schwartz@pearson.com>
2013-06-06 18:37:59 -0400

    Skill checkboxes

--- GeoLocateAndLearn/src/com/geolocateandlearn/PracticeChallengesActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/PracticeChallengesActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 
 public class PracticeChallengesActivity extends Activity {
+
+	private CheckBox skillListeningCheckbox;
+	private CheckBox skillSpeakingCheckbox;
+	private CheckBox skillReadingCheckbox;
+	private CheckBox skillWritingCheckbox;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_practice_challenge);
+		skillListeningCheckbox = (CheckBox) findViewById(R.id.listening_skills_selector);
+		skillSpeakingCheckbox = (CheckBox) findViewById(R.id.speaking_skills_selector);
+		skillReadingCheckbox = (CheckBox) findViewById(R.id.reading_skills_selector);
+		skillWritingCheckbox = (CheckBox) findViewById(R.id.writing_skills_selector);
 	}
 
 	@Override
@@ -20,6 +30,9 @@ public class PracticeChallengesActivity extends Activity {
 	}
 
 	public void resetSkills(MenuItem menuItem) {
-		// TODO reset all skills
+		skillListeningCheckbox.setChecked(false);
+		skillSpeakingCheckbox.setChecked(false);
+		skillReadingCheckbox.setChecked(false);
+		skillWritingCheckbox.setChecked(false);
 	}
 }
