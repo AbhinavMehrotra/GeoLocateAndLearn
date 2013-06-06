4386bae75c81410ebd825603c51593733f419e40
Steven H. Schwartz <steven.schwartz@pearson.com>
2013-06-06 05:29:04 -0400

    Updated app name and launcher icon.

--- GeoLocateAndLearn/gen/com/geolocateandlearn/R.java
+++ GeoLocateAndLearn/gen/com/geolocateandlearn/R.java
@@ -162,8 +162,9 @@ public final class R {
         public static final int header=0x7f020006;
         public static final int ic_action_search=0x7f020007;
         public static final int ic_launcher=0x7f020008;
-        public static final int me=0x7f020009;
-        public static final int nearby=0x7f02000a;
+        public static final int ic_launcher_2=0x7f020009;
+        public static final int me=0x7f02000a;
+        public static final int nearby=0x7f02000b;
     }
     public static final class id {
         public static final int about=0x7f080006;
