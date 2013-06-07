eb2d4386b9f0a003b3f0214c97c1dda1e950fc8e
Steven H. Schwartz <steven.schwartz@pearson.com>
2013-06-07 12:08:40 -0400

    Send dummy response to challenge

--- GeoLocateAndLearn/src/com/geolocateandlearn/SubmitChallengeResponseActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/SubmitChallengeResponseActivity.java
@@ -3,19 +3,41 @@ package com.geolocateandlearn;
 import com.geolocateandlearn.model.ChallengeResponse;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SubmitChallengeResponseActivity extends Activity {
 
 	public static final String EXTRA_RESPONSE = "EXTRA_RESPONSE";
-	
+
 	private ChallengeResponse response;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_submit_challenge_response);
-		response = (ChallengeResponse) getIntent().getSerializableExtra(EXTRA_RESPONSE);
+		response = (ChallengeResponse) getIntent().getSerializableExtra(
+				EXTRA_RESPONSE);
 		setTitle(response.getChallenge().getName());
+	}
+
+	public void sendResponseViaEmail(View v) {
+		final Intent sendEmailIntent = new Intent(Intent.ACTION_SEND);
+		sendEmailIntent.setType("message/rfc822");
+		String emailRecipient = "schwartz@alum.mit.edu";
+		sendEmailIntent.putExtra(Intent.EXTRA_EMAIL,
+				new String[] { emailRecipient });
+		String emailSubject = "Dummy challenge response";
+		sendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+		String emailText = "body of email";
+		sendEmailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
+		try {
+			startActivity(Intent.createChooser(sendEmailIntent, "Send mail..."));
+		} catch (android.content.ActivityNotFoundException ex) {
+			Toast.makeText(this, "There are no email clients installed.",
+					Toast.LENGTH_LONG).show();
+		}
 	}
 }
