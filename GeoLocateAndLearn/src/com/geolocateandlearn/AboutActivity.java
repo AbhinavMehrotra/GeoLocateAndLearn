5e5669eb1d5d426a980a3f5731ae280c61174815
Steven Schwartz <steven.schwartz@pearson.com>
2013-07-09 11:42:39 -0400

    Generics practice

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -1,5 +1,8 @@
 package com.geolocateandlearn;
 
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.geolocateandlearn.annotations.ArchitectureSegment;
 import com.geolocateandlearn.model.Challenge;
 import com.geolocateandlearn.model.ChallengeRecord;
@@ -32,9 +35,11 @@ public class AboutActivity extends Activity {
 				"Who is buried in the Lincoln Tunnel",
 				"Is Marginal Road important?",
 				"Where was the Upside-Down Building?");
-		ChallengeRecord<PracticeChallenge, Float> record1 = new ChallengeRecord<PracticeChallenge, Float>(
-				challenge1);
+		final ChallengeRecord<PracticeChallenge, Float> record1 = new ChallengeRecord<PracticeChallenge, Float>(
+				challenge1, new ConcurrentHashMap<Long, TreeSet<Byte>>());
 		outputBuilder.append(challenge1);
+		outputBuilder.append('\n');
+		outputBuilder.append(record1);
 
 		outputTextView.setText(outputBuilder.toString());
 	}
--- GeoLocateAndLearn/src/com/geolocateandlearn/model/ChallengeRecord.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/model/ChallengeRecord.java
@@ -1,10 +1,34 @@
 package com.geolocateandlearn.model;
 
 public class ChallengeRecord<T extends Challenge, U extends Number> {
+	private static final String NULL_ARGUMENT_TYPE_NAME = "Void";
 	private final T challenge;
+	private final String secondArgumentType;
 
-	public <V> ChallengeRecord(T challenge1) {
+	public <V> ChallengeRecord(T challenge1, V secondArgument) {
 		this.challenge = challenge1;
+		secondArgumentType = (secondArgument == null) ? NULL_ARGUMENT_TYPE_NAME
+				: secondArgument.getClass().getSimpleName();
+
+	}
+
+	public ChallengeRecord(T challenge1) {
+		this.challenge = challenge1;
+		secondArgumentType = NULL_ARGUMENT_TYPE_NAME;
+	}
+
+	public T getChallenge() {
+		return challenge;
+	}
+
+	public String getSecondArgumentType() {
+		return secondArgumentType;
+	}
+
+	@Override
+	public String toString() {
+		return "ChallengeRecord [challenge=" + challenge
+				+ ", secondArgumentType=" + secondArgumentType + "]";
 	}
 
 }
