11fb16293ebc7bb59149ea980a6351b3f501a7ed
Steven Schwartz <steven.schwartz@pearson.com>
2013-07-10 14:38:55 -0400

    NavigableSet

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -1,5 +1,7 @@
 package com.geolocateandlearn;
 
+import java.util.TreeSet;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,26 +29,58 @@ public class AboutActivity extends Activity {
 		final TextView outputTextView = (TextView) findViewById(R.id.about_screen_textView);
 		// TODO Put output here.
 
+		final String question1 = "Who is buried in the Lincoln Tunnel?";
+		final String question2 = "Is Marginal Road important?";
+		final String question3 = "Where was the Upside-Down Building?";
 		final PracticeChallenge challenge1 = new PracticeChallenge(
-				"The About Challenge",
-				"Who is buried in the Lincoln Tunnel?",
-				"Is Marginal Road important?",
-				"Where was the Upside-Down Building?");
+				"The About Challenge", question1, question2, question3);
 		final ChallengeRecord<PracticeChallenge> record1 = new ChallengeRecord<PracticeChallenge>(
 				challenge1);
 
-		int questionCount = 0;
-		for (String question : challenge1) {
-			questionCount++;
-		}
+		final TreeSet<String> questionSet = new TreeSet<String>();
 
-		outputBuilder.append(Integer.toBinaryString(questionCount));
-		outputBuilder.append("b questions.");
+		outputBuilder.append("Before: ");
+		outputBuilder.append(questionSet.size());
+		outputBuilder.append(" questions.\n");
+
+		questionSet.add(question1);
+		questionSet.add(question2);
+		questionSet.add(question1);
+		questionSet.add(question3);
+		questionSet.add(question3);
+		questionSet.add(question2);
+		questionSet.add(question1);
+		questionSet.add(question2);
+		questionSet.add(question1);
+		questionSet.add(question3);
+		questionSet.add(question3);
+		questionSet.add(question3);
+
+		outputBuilder.append("After: ");
+		outputBuilder.append(questionSet.size());
+		outputBuilder.append(" questions.\n");
+
+		outputBuilder.append("First: ");
+		outputBuilder.append(questionSet.first());
+		outputBuilder.append('\n');
+
+		outputBuilder.append("Last: ");
+		outputBuilder.append(questionSet.last());
+		outputBuilder.append('\n');
+
+		outputBuilder.append("Head L: ");
+		outputBuilder.append(questionSet.headSet("Which"));
+		outputBuilder.append('\n');
+
+		outputBuilder.append("Tail, INcluding: ");
+		outputBuilder.append(questionSet.tailSet(question3, true));
+		outputBuilder.append('\n');
+
+		outputBuilder.append("Tail, EXcluding: ");
+		outputBuilder.append(questionSet.tailSet(question3, false));
+		outputBuilder.append('\n');
 
 		outputTextView.setText(outputBuilder.toString());
-
-		final ChallengeRecord<PracticeChallenge> crClone = record1
-				.clone();
 	}
 
 	@Override
