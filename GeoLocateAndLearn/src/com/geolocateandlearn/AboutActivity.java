d59777379d08c4892ec3203f1eeb0cbfae4d33bd
Steven Schwartz <steven.schwartz@pearson.com>
2013-07-11 09:30:10 -0400

    housekeeping / refactoring

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -2,12 +2,9 @@ package com.geolocateandlearn;
 
 import java.util.PriorityQueue;
 import java.util.Random;
-import java.util.TreeSet;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.TextView;
 
 import com.geolocateandlearn.annotations.ArchitectureSegment;
@@ -17,7 +14,13 @@ import com.geolocateandlearn.model.PracticeChallenge;
 @ArchitectureSegment(segment = "core")
 public class AboutActivity extends Activity {
 
-	Intent intent;
+	private final static String question1 = "Who is buried in the Lincoln Tunnel?";
+	private final static String question2 = "Is Marginal Road important?";
+	private final static String question3 = "Where was the Upside-Down Building?";
+	private final static PracticeChallenge challenge1 = new PracticeChallenge(
+			"The About Challenge", question1, question2, question3);
+	private final static ChallengeRecord<PracticeChallenge> challengeRecord1 = new ChallengeRecord<PracticeChallenge>(
+			challenge1);
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,46 +29,38 @@ public class AboutActivity extends Activity {
 		loadOutput();
 	}
 
+	private class PQE implements Comparable<PQE> {
+		final private char title;
+		final private int order;
+
+		public PQE(char title, int order) {
+			this.title = title;
+			this.order = order;
+		}
+
+		public char getTitle() {
+			return title;
+		}
+
+		public int getOrder() {
+			return order;
+		}
+
+		public int compareTo(PQE that) {
+			if (this.order == that.order)
+				return 0;
+			else if (this.order < that.order)
+				return -1;
+			else
+				return 1;
+		}
+
+	}
+
 	private void loadOutput() {
 		final StringBuilder outputBuilder = new StringBuilder();
 		final TextView outputTextView = (TextView) findViewById(R.id.about_screen_textView);
 		// TODO Put output here.
-
-		final String question1 = "Who is buried in the Lincoln Tunnel?";
-		final String question2 = "Is Marginal Road important?";
-		final String question3 = "Where was the Upside-Down Building?";
-		final PracticeChallenge challenge1 = new PracticeChallenge(
-				"The About Challenge", question1, question2, question3);
-		final ChallengeRecord<PracticeChallenge> record1 = new ChallengeRecord<PracticeChallenge>(
-				challenge1);
-
-		class PQE implements Comparable<PQE> {
-			final private char title;
-			final private int order;
-
-			public PQE(char title, int order) {
-				this.title = title;
-				this.order = order;
-			}
-
-			public char getTitle() {
-				return title;
-			}
-
-			public int getOrder() {
-				return order;
-			}
-
-			public int compareTo(PQE that) {
-				if (this.order == that.order)
-					return 0;
-				else if (this.order < that.order)
-					return -1;
-				else
-					return 1;
-			}
-
-		}
 
 		outputBuilder.append("Constructing values:\n");
 		final PriorityQueue<PQE> pqueue = new PriorityQueue<PQE>();
@@ -91,12 +86,6 @@ public class AboutActivity extends Activity {
 		outputBuilder.append('\n');
 
 		outputTextView.setText(outputBuilder.toString());
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_about, menu);
-		return true;
 	}
 
 }
