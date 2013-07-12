237ccd009335b4cf42400071015d5dd70b0dbd8b
Steven H. Schwartz <steven.schwartz@pearson.com>
2013-07-11 23:22:04 -0400

    Currency

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -1,10 +1,14 @@
 package com.geolocateandlearn;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.TextView;
 
 import com.geolocateandlearn.annotations.ArchitectureSegment;
@@ -16,6 +20,8 @@ public class AboutActivity extends Activity {
 
 	private static final String PERIOD = ".";
 	private static final String FILE_TO_READ = "lorenipsum.txt";
+	private static final Currency currency = Currency.getInstance(Locale.CANADA_FRENCH);
+	private static final String currency_string = currency.getSymbol();
 	private final static String question1 = "Who is buried in the Lincoln Tunnel?";
 	private final static String question2 = "Is Marginal Road important?";
 	private final static String question3 = "Where was the Upside-Down Building?";
@@ -24,6 +30,8 @@ public class AboutActivity extends Activity {
 	private final static ChallengeRecord<PracticeChallenge> challengeRecord1 = new ChallengeRecord<PracticeChallenge>(
 			challenge1);
 
+	private static final long ONE_THIRD_SECOND = 333;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,27 +39,34 @@ public class AboutActivity extends Activity {
 		loadOutput();
 	}
 
+	final TimerTask incrementorTask = new TimerTask() {
+
+		@Override
+		public void run() {
+			final Handler handler = new Handler(Looper.getMainLooper());
+			handler.post(incrementingRunnable);
+		}
+
+	};
+
+	final Runnable incrementingRunnable = new Runnable() {
+
+		public void run() {
+			final TextView outputTextView = (TextView) findViewById(R.id.about_screen_textView);
+			int value = Integer.parseInt(((String) outputTextView.getText()).substring(1), 16) + 1;
+			outputTextView.setText(currency_string + Integer.toHexString(value));
+		}
+
+	};
+
 	private void loadOutput() {
 		final StringBuilder outputBuilder = new StringBuilder();
 		final TextView outputTextView = (TextView) findViewById(R.id.about_screen_textView);
 		// TODO Put output here.
 
-		final GregorianCalendar cal = new GregorianCalendar();
-		outputBuilder.append("Now: ").append(cal.toString())
-				.append("\n\n");
+		outputTextView.setText(currency_string + "0");
 
-		cal.clear();
-		outputBuilder.append("Zero: ").append(cal.toString())
-				.append("\n\n");
-
-		for (int i = 0; i < 24; i++) {
-			cal.add(Calendar.HOUR, 5);
-			cal.add(Calendar.MONTH, 2);
-			cal.add(Calendar.MINUTE, 27);
-			outputBuilder.append(i).append(": ").append(cal.toString())
-					.append("\n\n");
-		}
-
-		outputTextView.setText(outputBuilder.toString());
+		final Timer mytimer = new Timer(true);
+		mytimer.schedule(incrementorTask, ONE_THIRD_SECOND, ONE_THIRD_SECOND);
 	}
 }
