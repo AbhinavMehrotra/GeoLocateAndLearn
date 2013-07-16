8729f16f82ee47f3aefbba58f8c28c64a6158dbf
Steven H. Schwartz <steven.schwartz@pearson.com>
2013-07-16 17:16:19 -0400

    Signed-off-by: Steven H. Schwartz <steven.schwartz@pearson.com>

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -1,8 +1,6 @@
 package com.geolocateandlearn;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,97 +11,119 @@ import android.widget.TextView;
 import com.geolocateandlearn.annotations.ArchitectureSegment;
 
 @ArchitectureSegment(segment = "core")
-public class AboutActivity extends Activity implements FlukeListener {
+public class AboutActivity extends Activity {
 
-	public class FlukeUpdate {
+	public class Carousel implements Runnable {
 
-		private final Date timestamp;
+		public void run() {
+			// TODO Auto-generated method stub
 
-		public FlukeUpdate(Date instamp) {
-			timestamp = instamp;
-		}
-
-		public Date getStamp() {
-			return timestamp;
 		}
 
 	}
 
-	public class Fluke {
-		final Set<FlukeListener> listeners = new HashSet<FlukeListener>();
+	private static final Random random = new Random(System.currentTimeMillis());
 
-		public boolean addFlukeListener(FlukeListener newListener) {
-			return listeners.add(newListener);
+	public class Rider {
+		private final int id;
+
+		public Rider(int riderI) {
+			id = riderI;
 		}
 
-		public boolean removeFlukeListener(FlukeListener oldListener) {
-			return listeners.remove(oldListener);
-		}
+		public void getOnLine() {
+			// TODO Auto-generated method stub
 
-		public void modify(long l) {
-			final FlukeUpdate newUpdate = new FlukeUpdate(new Date());
-			for (FlukeListener listener : listeners) {
-				listener.update(newUpdate);
-			}
+			update(id + ": on line\n");
 		}
 
 	}
 
-	private Fluke fluke = null;
+	private static final int MAX_RIDERS = 20;
+
+	private static final int MAX_NEW_RIDER_DELAY = 1000;
+
+	private final Handler uiHandler = new Handler(Looper.getMainLooper());
+
+	private final StringBuffer outputHigherBuffer = new StringBuffer();
+	private TextView outputHigherTextView;
+	private final StringBuffer outputLowerBuffer = new StringBuffer();
+	private TextView outputLowerTextView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		fluke = new Fluke();
-		loadOutput();
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+		outputHigherTextView = (TextView) findViewById(R.id.about_screen_textView_higher);
+		outputLowerTextView = (TextView) findViewById(R.id.about_screen_textView_lower);
 	}
 
 	@Override
 	protected void onStart() {
+		// TODO Auto-generated method stub
 		super.onStart();
-		fluke.addFlukeListener(this);
-		fluke.modify(37L);
+		outputLowerBuffer.setLength(0);
+		run();
 	}
 
 	@Override
 	protected void onStop() {
-		fluke.removeFlukeListener(this);
+		// TODO Auto-generated method stub
 		super.onStop();
 	}
 
-	private void loadOutput() {
-		// final TextView outputTextView = (TextView)
-		// findViewById(R.id.about_screen_textView_lower);
-		// final StringBuffer output = new StringBuffer();
-		// TODO Put output here.
+	private class RiderLoader implements Runnable {
+		private Carousel carousel;
 
-
-		// outputTextView.setText(output.toString());
-	}
-
-	private Handler mHandler = new Handler(Looper.getMainLooper());
-
-	private class Updater implements Runnable {
-		private final FlukeUpdate updateData;
-
-		public Updater(FlukeUpdate flukeUpdate) {
-			updateData = flukeUpdate;
+		public RiderLoader(Carousel carousel) {
+			this.carousel = carousel;
 		}
 
 		public void run() {
-			final TextView outputTextView = (TextView) findViewById(R.id.about_screen_textView_lower);
-			outputTextView.setText("Updated at "
-					+ updateData.getStamp());
+			for (int riderI = 0; riderI < MAX_RIDERS; riderI++) {
+				new Rider(riderI).getOnLine();
+				try {
+					Thread.sleep(random.nextInt(MAX_NEW_RIDER_DELAY));
+				} catch (InterruptedException e) {
+					update(riderI + ": Time interrupted");
+				}
+			}
+		}
+	}
+
+	private void run() {
+		Carousel carousel = new Carousel();
+		new Thread(carousel).start();
+		new Thread(new RiderLoader(carousel)).start();
+	}
+
+	private class HigherUpdater implements Runnable {
+		private final String message;
+
+		public HigherUpdater(String message) {
+			this.message = message;
+		}
+
+		public void run() {
+			outputHigherBuffer.append(message);
+			outputHigherTextView.setText(outputHigherBuffer.toString());
 		}
 	};
 
-	public void update(FlukeUpdate flukeUpdate) {
-		mHandler.post(new Updater(flukeUpdate));
+	private class LowerUpdater implements Runnable {
+		private final String message;
+
+		public LowerUpdater(String message) {
+			this.message = message;
+		}
+
+		public void run() {
+			outputLowerBuffer.append(message);
+			outputLowerTextView.setText(outputLowerBuffer.toString());
+		}
+	};
+
+	public void update(String message) {
+		uiHandler.post(new LowerUpdater(message));
 	}
 }
--- GeoLocateAndLearn/src/com/geolocateandlearn/FlukeListener.java
+++
@@ -1,7 +0,0 @@
-package com.geolocateandlearn;
-
-import com.geolocateandlearn.AboutActivity.FlukeUpdate;
-
-interface FlukeListener {
-	void update(FlukeUpdate flukeUpdate);
-}
