ae7ebc8087d92ab67f147351f93cfc8024e9d3b8
Steven Schwartz <steven.schwartz@pearson.com>
2013-07-23 16:54:18 -0400

    Signed-off-by: Steven Schwartz <steven.schwartz@pearson.com>

--- GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
+++ GeoLocateAndLearn/src/com/geolocateandlearn/AboutActivity.java
@@ -1,15 +1,19 @@
 package com.geolocateandlearn;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Exchanger;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,8 +22,14 @@ import android.widget.TextView;
 
 import com.geolocateandlearn.annotations.ArchitectureSegment;
 
+@SuppressLint("SimpleDateFormat")
 @ArchitectureSegment(segment = "core")
 public class AboutActivity extends Activity {
+
+	private static final long RIDE_TIME = 300;
+
+	private static final SimpleDateFormat MMSS_TIME_FORMAT = new SimpleDateFormat(
+			"mm:ss");
 
 	public class Carousel implements Runnable {
 		private class BrassRingDispenser implements Runnable {
@@ -40,13 +50,13 @@ public class AboutActivity extends Activity {
 			}
 
 			public void grab(Rider rider) {
+				final Date eventTime = new Date();
 				brassRingCounter.countDown();
+				rider.setRingGrabTime(eventTime);
 			}
 		}
 
 		private static final int MAX_CONCURRENT_RIDERS = 6;
-
-		private static final long RIDE_TIME = 300;
 
 		private final Set<Rider> onHorses = new HashSet<Rider>();
 
@@ -65,27 +75,11 @@ public class AboutActivity extends Activity {
 		private final CyclicBarrier entranceGate = new CyclicBarrier(
 				MAX_CONCURRENT_RIDERS, gateFlag);
 
-		private Exchanger<Integer> idExchanger = new Exchanger<Integer>();
-
 		public void run() {
-			// TODO Auto-generated method stub
+			// TODO Rewrite code -- now utilizing entranceGate
 
-			new Thread(ringDispenser).start();
+			executor.execute(ringDispenser);
 			while (true) {
-				while (ridePermits.availablePermits() != 0) {
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						// no-op
-					}
-				}
-				updateHigher(" Start");
-				try {
-					Thread.sleep(RIDE_TIME);
-				} catch (InterruptedException e) {
-					updateHigher("Ride_interrupted");
-				}
-				updateHigher(" Stop");
 				for (Rider rider : new ArrayList<Rider>(onHorses)) {
 					rider.depart();
 				}
@@ -97,13 +91,6 @@ public class AboutActivity extends Activity {
 		}
 
 		public void board(Rider rider) {
-			Integer otherId;
-			try {
-				otherId = idExchanger.exchange(rider.getId());
-				updateHigher(" " + rider.getId() + "_EXCHANGE_" + otherId);
-			} catch (InterruptedException e) {
-				updateHigher(" " + rider.getId() + "_EXCHANGE_FAIL");
-			}
 			ringDispenser.grab(rider);
 			onHorses.add(rider);
 		}
@@ -122,15 +109,22 @@ public class AboutActivity extends Activity {
 
 	}
 
-	private static final Random random = new Random(System.currentTimeMillis());
+	private static final Random random = new Random(
+			System.currentTimeMillis());
 
 	private class Rider implements Runnable {
 		private final int id;
 		private final Carousel carousel;
+		private Date ringGrabTime;
 
 		public Rider(int riderI, Carousel carousel) {
 			id = riderI;
 			this.carousel = carousel;
+		}
+
+		public void setRingGrabTime(Date newRingGrabTime) {
+			// TODO Auto-generated method stub
+			this.ringGrabTime = newRingGrabTime;
 		}
 
 		public int getId() {
@@ -140,52 +134,30 @@ public class AboutActivity extends Activity {
 		public void run() {
 			updateLower(" " + id + "_arrive");
 			arrive();
+			updateLower(" " + id + "_ride");
+			Thread.sleep(RIDE_TIME);
+			// TODO HERE
+			exitGate.await();
+			depart();
+	
 		}
 
 		private void arrive() {
 			carousel.approachGate(this);
 			carousel.board(this);
-			updateLower(" " + id + "_ride");
-		}
+				}
 
 		public void depart() {
 			carousel.debark(this);
 			updateLower(" " + id + "_leave");
+			if (ringGrabTime == null) {
+				updateHigher(" " + id + "_NO_RING");
+			} else {
+				updateHigher(" " + id + "_ring_at_"
+						+ MMSS_TIME_FORMAT.format(ringGrabTime));
+			}
 		}
 
-	}
-
-	private static final int MAX_RIDERS = 21;
-
-	private static final int MAX_NEW_RIDER_DELAY = 1000;
-
-	private final Handler uiHandler = new Handler(Looper.getMainLooper());
-
-	private final StringBuffer outputHigherBuffer = new StringBuffer();
-	private TextView outputHigherTextView;
-	private final StringBuffer outputLowerBuffer = new StringBuffer();
-	private TextView outputLowerTextView;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about);
-		outputHigherTextView = (TextView) findViewById(R.id.about_screen_textView_higher);
-		outputLowerTextView = (TextView) findViewById(R.id.about_screen_textView_lower);
-	}
-
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		outputLowerBuffer.setLength(0);
-		run();
-	}
-
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
 	}
 
 	private class RiderLoader implements Runnable {
@@ -196,21 +168,58 @@ public class AboutActivity extends Activity {
 		}
 
 		public void run() {
+			int cumulativeDelayMsec = 0;
 			for (int riderI = 0; riderI < MAX_RIDERS; riderI++) {
-				new Thread(new Rider(riderI, carousel)).start();
-				try {
-					Thread.sleep(random.nextInt(MAX_NEW_RIDER_DELAY));
-				} catch (InterruptedException e) {
-					updateLower(riderI + ": Time interrupted");
-				}
+				cumulativeDelayMsec += random
+						.nextInt(MAX_NEW_RIDER_DELAY);
+				executor.schedule(new Rider(riderI, carousel),
+						cumulativeDelayMsec, TimeUnit.MILLISECONDS);
 			}
 		}
 	}
 
+	private static final int MAX_RIDERS = 21;
+
+	private static final int MAX_NEW_RIDER_DELAY = 1000;
+
+	private final Handler uiHandler = new Handler(
+			Looper.getMainLooper());
+
+	private final StringBuffer outputHigherBuffer = new StringBuffer();
+	private TextView outputHigherTextView;
+	private final StringBuffer outputLowerBuffer = new StringBuffer();
+	private TextView outputLowerTextView;
+
+	private ScheduledThreadPoolExecutor executor;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		outputHigherTextView = (TextView) findViewById(R.id.about_screen_textView_higher);
+		outputLowerTextView = (TextView) findViewById(R.id.about_screen_textView_lower);
+		executor = new ScheduledThreadPoolExecutor(10);
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		outputHigherBuffer.setLength(0);
+		outputLowerBuffer.setLength(0);
+		run();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
 	private void run() {
-		Carousel carousel = new Carousel();
-		new Thread(carousel).start();
-		new Thread(new RiderLoader(carousel)).start();
+		final Carousel carousel = new Carousel();
+		executor.execute(carousel);
+		executor.execute(new RiderLoader(carousel));
 	}
 
 	private class HigherUpdater implements Runnable {
